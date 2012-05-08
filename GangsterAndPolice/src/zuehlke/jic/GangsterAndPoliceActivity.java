60c2d07f2548761fed69edeff80f92ffb9644fea
fork.ch@gmail.com <fork.ch@gmail.com@5448b0a4-250c-c8d1-c4fc-1c12b9ed871a>
2012-05-08 06:25:23 +0000

    added project

---
+++ GangsterAndPolice/src/zuehlke/jic/GangsterAndPoliceActivity.java
@@ -0,0 +1,13 @@
+package zuehlke.jic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GangsterAndPoliceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
